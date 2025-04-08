e40274ab00174eadb4a3a2b26a7b11a5acf42c31
eduardahermannalmeida <dudahdealmeida29@gmail.com>
2025-04-08 19:47:28 -0300

    Atualizações

--- Aula16EHA/aula16-project/src/components/profile/index.jsx
+++ Aula16EHA/aula16-project/src/components/profile/index.jsx
@@ -1,14 +1,17 @@
 import "./styles.css";
+import background from "../../assets/profile_background.png";
+import pfp from "../../assets/leslie_icon.png"
 import { PencilSimpleLine } from "@phosphor-icons/react";
 
-export function Profile({ background, pfp, name, job }) {
+export function Profile({ name, job }) {
   return (
     <div className="profile">
-      <img src={background} alt="Plano de Fundo do Usuário" />
-      <img src={pfp} alt="Foto de Perfil do Usuário" />
+      <img className="bg" src={background} alt="Plano de Fundo do Usuário" />
+      <img className="pfp" src={pfp} alt="Foto de Perfil do Usuário" />
       <h1 className="name"> {name} </h1>
       <h2 className="job"> {job} </h2>
-      <button type="button"> <PencilSimpleLine size={14.12} />Editar seu perfil </button>
+      <hr/>
+      <button className="btn" type="button"> <PencilSimpleLine size={14.12} />Editar seu perfil </button>
     </div>
   );
 }
